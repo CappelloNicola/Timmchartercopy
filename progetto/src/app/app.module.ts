367ce48b783d8eef8f93a146ca29c69e52c0ec8e
riioKen <domenicovitale69@gmail.com>
2023-03-29 16:05:48 +0200

    Aggiunta mappe
    npm install --save leaflet-routing-machine
    npm install --save leaflet
    Aggiunta del css di leaflet in angular.json
    "./node_modules/leaflet/dist/leaflet.css"

--- progetto/src/app/app.module.ts
+++ progetto/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Map1Component } from './components/map1/map1.component';
+import { Map2Component } from './components/map2/map2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Map1Component,
+    Map2Component
   ],
   imports: [
     BrowserModule,
---
+++ progetto/src/app/components/map2/map2.component.ts
@@ -0,0 +1,32 @@
+import { Component, AfterViewInit  } from '@angular/core';
+import * as L from 'leaflet';
+import 'leaflet-routing-machine';
+
+@Component({
+  selector: 'app-map2',
+  templateUrl: './map2.component.html',
+  styleUrls: ['./map2.component.css']
+})
+export class Map2Component implements AfterViewInit {
+  private map:any;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+}
