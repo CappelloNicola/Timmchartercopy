9f529c2a9ccac84702822b274544b4aa77278567
monicasebastiano05 <monica.sebastiano.3fedin@hotmail.com>
2023-03-30 12:54:54 +0200

    assafa

--- progetto/src/app/app.module.ts
+++ progetto/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
