86d130e1fd1fbee276222fa7f042cbe44f0b6392
Vladyslav Okopnyy <harmfulcati@gmail.com>
2023-03-30 13:25:28 +0200

    Cookie and Porto its done.

--- progetto/src/app/app.component.ts
+++ progetto/src/app/app.component.ts
@@ -7,25 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'progetto';
-
-  showCookiePopup: boolean = true;
-  showCookiePopupF: boolean = true;
-  showReservation: boolean = false;
-
-  constructor() {}
-
-  hideCookiePopup() {
-    this.showCookiePopup = false;
-  }
-
-  hideCookiePopupF() {
-    this.showCookiePopupF = false;
-  }
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.showReservation = true;
-    }, 50);
-  }
-
 }
--- progetto/src/app/components/cookie/cookie.component.ts
+++ progetto/src/app/components/cookie/cookie.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./cookie.component.css']
 })
 export class CookieComponent {
+  showCookiePopup: boolean = true;
+  showCookiePopupF: boolean = true;
+  showReservation: boolean = false;
 
+  constructor() {}
+
+  hideCookiePopup() {
+    this.showCookiePopup = false;
+  }
+
+  hideCookiePopupF() {
+    this.showCookiePopupF = false;
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.showReservation = true;
+    }, 50);
+  }
 }
